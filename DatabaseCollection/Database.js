1f4cb9bfc5b2fab6a1eb4233bab03f161a828132
ashishkanojia <kanojiaashish02@gmail.com>
2024-07-28 23:22:58 +0530

    fixing bug in database file

--- DatabaseCollection/Database.js
+++ DatabaseCollection/Database.js
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 export const databaseConnection = async () => {
   try {
-    const { connection } = await mongoose.connect(process.env.MONGO_URI);
+    const { connection } = await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 30000, // Increase the timeout
+      socketTimeoutMS: 45000,
+    });
     console.log(
       `this connection is made for database with host name is ${connection.host}`
     );
   } catch (error) {
-    console.log("This error is come from database connection file",error);
+    console.log("This error is come from database connection file", error);
   }
 };
 
