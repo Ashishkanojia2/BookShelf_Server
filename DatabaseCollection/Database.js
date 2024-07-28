2744b3ef846ae2f29d41c8a917b9362745aa01fb
ashishkanojia <kanojiaashish02@gmail.com>
2024-07-28 16:57:18 +0530

    fix bug in database file

--- DatabaseCollection/Database.js
+++ DatabaseCollection/Database.js
@@ -3,10 +3,32 @@ import mongoose from "mongoose";
 export const databaseConnection = async () => {
   try {
     const { connection } = await mongoose.connect(process.env.MONGO_URI);
-    // console.log(
-    //   // `this connection is made for database with host name is ${connection.host}`
-    // );
+    console.log(
+      `this connection is made for database with host name is ${connection.host}`
+    );
   } catch (error) {
     console.log("This error is come from database connection file",error);
   }
 };
+
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// dotenv.config(); // Ensure dotenv is configured correctly
+
+// export const databaseConnection = async () => {
+//   try {
+//     const mongoURI = process.env.MONGO_URI;
+//     if (!mongoURI) {
+//       throw new Error("MONGO_URI is not defined");
+//     }
+
+//     const { connection } = await mongoose.connect(mongoURI);
+
+//     console.log(
+//       `This connection is made for database with host name: ${connection.host}`
+//     );
+//   } catch (error) {
+//     console.log("This error is coming from database connection file", error);
+//   }
+// };
