46cb579cbabf6fbd548e138c700f29b342a2dde5
ashishkanojia <kanojiaashish02@gmail.com>
2024-08-20 23:43:46 +0530

    add data in login response

--- utils/SendToken.js
+++ utils/SendToken.js
@@ -9,6 +9,7 @@ export const sendToken = (res, statusCode, userdata, message) => {
     address: userdata.address,
     verified: userdata.verified,
     otp: userdata.otp,
+    createdAt : userdata.createdAt
   };
 
   const token = jwt.sign({ _id: userdata._id }, process.env.JWT_SECRET_KEY);
