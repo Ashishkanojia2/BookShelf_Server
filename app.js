d4109c2f2fbc17d1803585fac79132e06cc86ba7
ashishkanojia <kanojiaashish02@gmail.com>
2024-09-10 00:30:57 +0530

    changes in app.js file

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ import feedbackRouter from "./Routers/FeedbackRouters.js";
 export const app = express();
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
-import cors  from "cors"
+import cors from "cors";
 
 //YAHA PR USER ROUTER FILE KO IMPORT KENA HIA
 // COOKIE PARSER KO BHEE IMPORT KRNA HAI
 app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+//DON'T REMOVE THIS **************************************************
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.use(
   fileUpload({
@@ -19,7 +23,12 @@ app.use(
     useTempFiles: true, // FOR THIS TEMP FOLDER WILL CREATE AND PHOTO WILL BE STORE ON TEMPEORY BEASES
   })
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // Allow all origins or specify the mobile app URL
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
 app.use("/api/v1/user", User);
 app.use("/api/v1/book", bookRouter);
 app.use("/api/v1/feedback", feedbackRouter);
